8426d92ac96f89678d0fe5be98b2883f3a3e904c
MercyPalleti <palletimercy1@gmail.com>
2025-01-24 15:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,42 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let roman = '';
+
+    for (let key in obj) {
+        const [symbol, value] = obj[key];
+
+        // Determine how many times the current Roman numeral fits into the number
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
+        }
+
+        // Handle special cases (subtractive notation)
+        if (key == 2 && num >= 900) { // 900 -> CM
+            roman += 'CM';
+            num -= 900;
+        } else if (key == 2 && num >= 400) { // 400 -> CD
+            roman += 'CD';
+            num -= 400;
+        } else if (key == 4 && num >= 90) { // 90 -> XC
+            roman += 'XC';
+            num -= 90;
+        } else if (key == 4 && num >= 40) { // 40 -> XL
+            roman += 'XL';
+            num -= 40;
+        } else if (key == 6 && num >= 9) { // 9 -> IX
+            roman += 'IX';
+            num -= 9;
+        } else if (key == 6 && num >= 4) { // 4 -> IV
+            roman += 'IV';
+            num -= 4;
+        }
+    }
+
+    return roman;
+}
+
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
